19475f5222ba5952889d8b29e541e7a203ee7430
Uriel Salgado <uri.auss@gmail.com>
2026-05-09 23:20:47 -0600

    Update ej2.py

--- ej2.py
+++ ej2.py
@@ -1,5 +1,10 @@
-#diseña un algoritmo recursivo que imprima todos los elementos de un arreglo
+def imprimir_arreglo(arreglo, indice=0):
+    if indice == len(arreglo):
+        return
+    
+    print(arreglo[indice])
+    
+    imprimir_arreglo(arreglo, indice + 1)
 
-def arreglo(arreglo, indi= 0):
-    if indi = len(arreglo):
-        return 
+mi_lista = [10, 20, 30, 40, 50]
+imprimir_arreglo(mi_lista)
